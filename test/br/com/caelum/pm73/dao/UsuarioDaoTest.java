a4c6f549ca9120a1aea73439b42dead7dd75ccf2
Jorge Hech <diordiaum@hotmail.com>
2019-07-10 18:39:13 -0300

    criando primeiro teste

--- test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
+++ test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
@@ -1,13 +1,22 @@
 package br.com.caelum.pm73.dao;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.junit.Test;
 import org.mockito.Mockito;
+
+import br.com.caelum.pm73.dominio.Usuario;
 
 public class UsuarioDaoTest {
 	
 	@Test
 	public void deveEncontrarPeloNomeEEmailMockado() {
 		Session session = Mockito.mock(Session.class);
+		Query query = Mockito.mock(Query.class);
+		UsuarioDao usuarioDao = new UsuarioDao(session);
+		
+		Usuario usuario = new Usuario("João da Silva", "joao@dasilva.com.br");
+		
+		Mockito.when(session.createQuery(sql))
 	}
 }
