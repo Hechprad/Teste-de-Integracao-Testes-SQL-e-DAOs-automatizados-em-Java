85d6fbd7c2cc670ddb67ef597bcdeac842153842
Jorge Hech <diordiaum@hotmail.com>
2019-07-11 12:07:01 -0300

    teste pronto

--- test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
+++ test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
@@ -2,33 +2,30 @@ package br.com.caelum.pm73.dao;
 
 import static org.junit.Assert.assertEquals;
 
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import br.com.caelum.pm73.dominio.Usuario;
 
 public class UsuarioDaoTest {
 	
 	@Test
-	public void deveEncontrarPeloNomeEEmailMockado() {
-		Session session = Mockito.mock(Session.class);
-		Query query = Mockito.mock(Query.class);
-		UsuarioDao usuarioDao = new UsuarioDao(session);
-		
-		Usuario usuario = new Usuario("João da Silva", "joao@dasilva.com.br");
-		
-		String sql = "from Usuario u where u.nome = :nome and u.email = :email";
-		
-		Mockito.when(session.createQuery(sql)).thenReturn(query);
-		Mockito.when(query.uniqueResult()).thenReturn(usuario);
-		Mockito.when(query.setParameter("nome", "João da Silva")).thenReturn(query);
-		Mockito.when(query.setParameter("email", "joao@dasilva.com.br")).thenReturn(query);
-		
-		Usuario usuarioDoBanco = usuarioDao.porNomeEEmail("João da Silva", "joao@dasilva.com.br");
-		
-		assertEquals(usuario.getNome(), usuarioDoBanco.getNome());
-		assertEquals(usuario.getEmail(), usuarioDoBanco.getEmail());
-	}
+    public void deveEncontrarPeloNomeEEmail() {
+        Session session = new CriadorDeSessao().getSession();
+        UsuarioDao usuarioDao = new UsuarioDao(session);
+
+        // criando um usuario e salvando antes de
+        // chamar o método porNomeEEmail
+        Usuario novoUsuario = new Usuario("João da Silva", "joao@dasilva.com.br");
+        usuarioDao.salvar(novoUsuario);
+
+        // agora buscamos no banco
+        Usuario usuarioDoBanco = usuarioDao.porNomeEEmail("João da Silva", "joao@dasilva.com.br");
+
+        assertEquals("João da Silva", usuarioDoBanco.getNome());
+        assertEquals("joao@dasilva.com.br", usuarioDoBanco.getEmail());
+
+        session.close();
+        // lembrar de executar a classe "CriaTabelas" antes de executar o teste
+    }
 }
