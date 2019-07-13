f98cad2f56a4d414cbcfaaf2f2590d57957e5499
Jorge Hech <diordiaum@hotmail.com>
2019-07-13 14:05:51 -0300

    novo teste de leilões não encerrados

--- test/br/com/caelum/pm73/dao/LeilaoDaoTest.java
+++ test/br/com/caelum/pm73/dao/LeilaoDaoTest.java
@@ -1,9 +1,14 @@
 package br.com.caelum.pm73.dao;
 
+import static org.junit.Assert.assertEquals;
+
 import org.hibernate.Session;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import br.com.caelum.pm73.dominio.Leilao;
+import br.com.caelum.pm73.dominio.Usuario;
 
 public class LeilaoDaoTest {
 
@@ -25,6 +30,18 @@ public class LeilaoDaoTest {
 	
 	@Test
 	public void deveContarLeiloesNaoEncerrados() {
-		leilaoDao.total();
+		Usuario mario = new Usuario("Mario Costa", "mario@costa.com.br");
+		
+		Leilao ativo = new Leilao("Geladeira nova", 1500.0, mario, false);
+		Leilao encerrado = new Leilao("XBox", 700.0, mario, false);
+		encerrado.encerra();
+		
+		usuarioDao.salvar(mario);
+		leilaoDao.salvar(ativo);
+		leilaoDao.salvar(encerrado);
+
+		long total = leilaoDao.total();
+		
+		assertEquals(1, total);
 	}
 }
