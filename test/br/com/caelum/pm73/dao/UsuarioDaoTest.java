ddcfe1fa9a754c00e09270844bf98c0e1f2a1fc2
Jorge Hech <diordiaum@hotmail.com>
2019-07-13 13:56:56 -0300

    tirando o 'this.'

--- test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
+++ test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
@@ -11,41 +11,41 @@ import org.junit.Test;
 import br.com.caelum.pm73.dominio.Usuario;
 
 public class UsuarioDaoTest {
-	
+
 	private Session session;
 	private UsuarioDao usuarioDao;
 
 	@Before
 	public void setUp() {
-		this.session = new CriadorDeSessao().getSession();
-        this.usuarioDao = new UsuarioDao(session);
+		session = new CriadorDeSessao().getSession();
+		usuarioDao = new UsuarioDao(session);
 	}
-	
+
 	@After
 	public void closeSession() {
 		// fechando conexão do BD
 		session.close();
 	}
-	
+
 	@Test
-    public void deveEncontrarPeloNomeEEmail() {
-        // criando um usuario e salvando antes de
-        // chamar o método porNomeEEmail
-        Usuario novoUsuario = new Usuario("João da Silva", "joao@dasilva.com.br");
-        usuarioDao.salvar(novoUsuario);
+	public void deveEncontrarPeloNomeEEmail() {
+		// criando um usuario e salvando antes de
+		// chamar o método porNomeEEmail
+		Usuario novoUsuario = new Usuario("João da Silva", "joao@dasilva.com.br");
+		usuarioDao.salvar(novoUsuario);
 
-        // agora buscamos no banco
-        Usuario usuarioDoBanco = usuarioDao.porNomeEEmail("João da Silva", "joao@dasilva.com.br");
+		// agora buscamos no banco
+		Usuario usuarioDoBanco = usuarioDao.porNomeEEmail("João da Silva", "joao@dasilva.com.br");
 
-        assertEquals("João da Silva", usuarioDoBanco.getNome());
-        assertEquals("joao@dasilva.com.br", usuarioDoBanco.getEmail());
-        // lembrar de executar a classe "CriaTabelas" antes de executar o teste
-    }
-	
+		assertEquals("João da Silva", usuarioDoBanco.getNome());
+		assertEquals("joao@dasilva.com.br", usuarioDoBanco.getEmail());
+		// lembrar de executar a classe "CriaTabelas" antes de executar o teste
+	}
+
 	@Test
 	public void deveRetornarNullSeUsuarioNaoExistir() {
 		Usuario usuarioDoBanco = usuarioDao.porNomeEEmail("Joaquim Peixoto", "joaquinzeira@email.com");
-		
+
 		assertNull(usuarioDoBanco);
 	}
 }
